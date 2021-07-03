8ecb330ae36b584935634c3eafea9e831810dcfd
toutchykipik <58473279+antho635@users.noreply.github.com>
2021-07-04 01:55:36 +0200

    icn head marge 20px

---
+++ inc/header.php
@@ -0,0 +1,82 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>DI DIO Anthony Développeur web</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/logo1.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a2a69c8064.js" crossorigin="anonymous"></script>
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/layout.css" rel="stylesheet">
+
+    <meta name="google-site-verification" content="BuNUtZVAVZQUBGeWqmBsjTk-W2kad1V5pSm5Zs-q1NY" />
+</head>
+
+<body id="page-1">
+
+    <!-- ======= NAVIGUATION MOBILE ======= -->
+    <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+    <!-- ======= HEADER ======= -->
+    <header class="header">
+        <div class="d-flex flex-column">
+
+            <!--
+            <div class="profile">
+                <img src="assets/img/profile-img.jpg" alt="photo de profile" class="img-fluid rounded-circle">
+                <h1 class="text-light" id="titre1"><a href="index.html">Développeur web & web mobile</a></h1>
+                <div class="social-links mt-3 text-center">
+                    <a href="https://github.com/antho635" class="icn-scl" alt="GITHUB,QR-code en bas"><i
+                            class="icofont-github"></i></a>
+                    <a href="https://www.linkedin.com/in/anthony-di-dio-46090a153/" class="icn-scl" alt="LINKEDIN,QR-code en bas"><i class="bx bxl-linkedin"></i></a>
+                </div>
+            </div>
+        -->
+
+            <nav class="nav-menu">
+                <ul>
+                    <li class="active"><a href="index.html"><i class="bx bx-home"></i> <span> Acceuil</span></a></li>
+                    <li><a href="portfolio-details.html#portfolio"><i class="bx bx-user"></i> <span>
+                                Portfolio</span></a>
+                    </li>
+                    <li><a href="#resume"><i class="bx bx-file-blank"></i> <span> Curriculum Vitae</span></a></li>
+                    <!--<li><a href="certificat.html#certif"><i class="fas fa-graduation-cap"></i> <span> Certificats
+                                OpenClassrooms</span></a></li>-->
+                    <li><a href="#services"><i class="bx bx-server"></i> Environnement</a></li>
+                    <li><a href="#contact"><i class="bx bx-envelope"></i> Contact</a></li>
+                </ul>
+            </nav>
+            <!-- .nav-menu -->
+            <div class="profile">
+                <div class="social-links mt-3 text-center">
+                    <a href="https://github.com/antho635" class="icn-scl" alt="GITHUB,QR-code en bas"><i class="icofont-github"></i></a>
+                    <a href="https://www.linkedin.com/in/anthony-di-dio-46090a153/" class="icn-scl" alt="LINKEDIN,QR-code en bas">
+                        <i class="bx bxl-linkedin"></i></a>
+                </div>
+            </div>
+            <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+        </div>
+    </header>
+    <!-- HEADER -->
